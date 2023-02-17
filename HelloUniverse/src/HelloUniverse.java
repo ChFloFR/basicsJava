730d6ed2ffdd5a90e8c43ce5ed7efa979b8c9b98
FlorentN <floneuville@gmail.com>
2023-02-17 20:00:46 +0100

    first commit

---
+++ HelloUniverse/src/HelloUniverse.java
@@ -0,0 +1,18 @@
+public class HelloUniverse {
+    public static void main(String... args){
+        System.out.println("Hello Universe");
+
+        int huitiemeHabitantsDansLeMonde;
+        byte ageDuCapitaine;
+        short paleolothiqueSuperieur;
+        long quartHabitantsDansLeMonde;
+
+        float temperatureActuelle;
+        double megaFloat;
+
+        boolean vraiOuFaux;
+        
+        char initialePrenom;
+
+    }
+}
